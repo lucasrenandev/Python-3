b47a2911eeab5588dd18a9f29b5de58a4577c8f6
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-13 11:09:01 -0300

    create ex029

---
+++ curso python/exercicios/ex029/custodaviagem.py
@@ -0,0 +1,14 @@
+distancia = float(input("Qual a distancia da viagem? "))
+print("Você está prestes a começar uma viagem de {}Km.".format(distancia))
+if distancia <= 200:
+    preco = distancia * 0.50
+else:
+    preco = distancia * 0.45
+print("E o preço da passagem será de R${:.2f}".format(preco))
+
+#distancia = float(input("Qual a distancia da viagem? "))
+#print("Você está prestes a começar uma viagem de {}Km.".format(distancia))
+#preco = distancia * 0.50 if distancia <= 200 else distancia * 0.45
+#print("E o valor da viagem será de R${:.2f}".format(preco))
+
+
