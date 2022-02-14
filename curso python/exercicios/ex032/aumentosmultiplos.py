32417a4ccadce8fe3a768e0cc4cd062a28536806
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-13 21:24:45 -0300

    Update aumentosmultiplos.py

--- curso python/exercicios/ex032/aumentosmultiplos.py
+++ curso python/exercicios/ex032/aumentosmultiplos.py
@@ -3,4 +3,4 @@ if salario <= 1250:
     novo = salario + (salario * 15 / 100)
 else:
     novo = salario + (salario * 10 / 100)
-print("O funcionario que ganhava R${:.2f} passará a ganhar R${:.2f} agora".format(salario, novo))
+print("O funcionário que ganhava R${:.2f} passará a ganhar R${:.2f} agora".format(salario, novo))
