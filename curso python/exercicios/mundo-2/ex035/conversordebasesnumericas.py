607b92a97a75bc901c79b4ec357e939b548abc9e
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-14 15:58:29 -0300

    Update conversordebasesnumericas.py

--- curso python/exercicios/mundo-2/ex035/conversordebasesnumericas.py
+++ curso python/exercicios/mundo-2/ex035/conversordebasesnumericas.py
@@ -3,7 +3,6 @@ print('''Escolha uma das bases para conversão:
 [ 1 ] converter para BINÁRIO
 [ 2 ] converter para OCTAL
 [ 3 ] conveter para HEXADECIMAL''')
-
 opcao = int(input("Sua opção: "))
 if opcao == 1:
     print("{} convertido em BINÁRIO é igual a {}".format(num, bin(num) [2:]))
