bb63efd0816ba8853f7034cc293455639d2c318d
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-16 15:13:07 -0300

    Update gerenciadordepagamentos.py

--- curso python/exercicios/mundo-2/ex042/gerenciadordepagamentos.py
+++ curso python/exercicios/mundo-2/ex042/gerenciadordepagamentos.py
@@ -1,21 +1,21 @@
 print('{:=^40}'.format(' LOJAS GUANABARA '))
 preco = float(input('Preço das compras: '))
 print(''' FORMAS DE PAGAMENTO
-[ 1 ] á vista dinheiro/cheque
-[ 2 ] á vista cartão
+[ 1 ] à vista dinheiro/cheque
+[ 2 ] à vista cartão
 [ 3 ] 2x no cartão
 [ 4 ] 3x ou mais no cartão ''')
 opcao = int(input("Sua opção? "))
 if opcao == 1:
     total = preco - (preco * 10 / 100)
-    print('á vista o produto sai por R${:.2f}'.format(total))
+    print('À vista o produto sai por R${:.2f}'.format(total))
 elif opcao == 2:
         total = preco - (preco * 5 / 100)
-        print("á vista no cartao o produto sai por R${:.2f}".format(total))
+        print("À vista no cartão o produto sai por R${:.2f}".format(total))
 elif opcao == 3:
     total = preco 
     parcela = total / 2
-    print('Sua compra sera parcelada em 2x de R${:.2f}'.format(parcela))
+    print('Sua compra sera parcelada em 2x de R${:.2f} sem JUROS'.format(parcela))
 elif opcao == 4:
     total = preco + (preco * 20 / 100)
     totalparcelas = int(input('Quantas parcelas? '))
@@ -25,5 +25,5 @@ else:
     total = 0
     print('OPÇÂO INVÁLIDA. tente novamente!')
 
-print('Sua compra de {} vai custar {} no final'.format(preco, total))
+print('Sua compra de R${:.2f} vai custar R${:.2f} no final'.format(preco, total))
 
