25c3ed648362635b5068ce1b1ff30f707b5e3dc5
Lucas Renan <lucasrenandns96@gmail.com>
2022-02-20 23:02:51 -0300

    Update progressaoaritmetica.py

--- curso python/exercicios/mundo-2/ex049/progressaoaritmetica.py
+++ curso python/exercicios/mundo-2/ex049/progressaoaritmetica.py
@@ -3,4 +3,4 @@ razão = int(input('Razão: '))
 décimo = primeiro + (10 - 1) * razão
 for c in range(primeiro, décimo + razão, razão):
     print('{}'.format(c), end=' --> ')
-print('ACABOU')
+print('ACABOU !!!')
