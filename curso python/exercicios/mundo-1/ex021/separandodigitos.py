8dd923d7a17a31a113656897071097a96813abe9
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:20:42 -0300

    Update separandodigitos.py

--- curso python/exercicios/mundo-1/ex021/separandodigitos.py
+++ curso python/exercicios/mundo-1/ex021/separandodigitos.py
@@ -1,9 +1,12 @@
+from time import sleep
+
 num = int(input('Informe um número: '))
 u = num // 1 % 10
 d = num // 10 % 10
 c = num // 100 % 10
 m = num // 1000 % 10
 print('Analisando o número {}'.format(num))
+sleep(2)
 print('unidade: {}'.format(u))
 print('dezena: {}'.format(d))
 print('Centena: {}'.format(c))
