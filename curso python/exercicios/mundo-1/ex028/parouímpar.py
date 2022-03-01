2bf467537cf8922f52cdb9d23c0a76a21063964f
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 14:18:37 -0300

    Update parouímpar.py

--- curso python/exercicios/mundo-1/ex028/parouímpar.py
+++ curso python/exercicios/mundo-1/ex028/parouímpar.py
@@ -1,6 +1,6 @@
 num = int(input("Digite um número qualquer: "))
 resultado = num % 2
 if resultado == 0:
-    print("o número {} é PAR".format(num))
+    print("O número {} é PAR".format(num))
 else:
     print("O número {} é ÍMPAR".format(num))
