60c264a8fb3749c143eb27be22bda90b3429cee9
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 14:26:50 -0300

    Update somandodoisnumeros.py

--- curso python/exercicios/mundo-1/ex001b/somandodoisnumeros.py
+++ curso python/exercicios/mundo-1/ex001b/somandodoisnumeros.py
@@ -1,4 +1,4 @@
 n1 = int(input('Digite um valor: '))
 n2 = int(input('Digite outro: '))
 s = n1 + n2
-print('a soma entre {} e {} é igual a {}!'.format(n1, n2, s))
+print('A soma entre {} e {} é igual a {}!'.format(n1, n2, s))
