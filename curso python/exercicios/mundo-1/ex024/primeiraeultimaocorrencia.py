d1188b8abee5132cb990a495a1d433ecc0a8728a
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:26:59 -0300

    Update primeiraeultimaocorrencia.py

--- curso python/exercicios/mundo-1/ex024/primeiraeultimaocorrencia.py
+++ curso python/exercicios/mundo-1/ex024/primeiraeultimaocorrencia.py
@@ -1,7 +1,4 @@
-from re import A
-
-
-frase = str(input('Digite uma frase: ')).upper()
-print('A letra a aparece {} vezes na frase'.format(frase.count('A')))
-print('Ela aparece a primeira vez na posição {}'.format(frase.find('A')))   ##find(encontrar)
-print('Ela aparece a ultima vez na posição {}'.format(frase.rfind('A')))
+frase = str(input('Digite uma frase: ')).upper().strip()
+print('A letra A apareceu {} vezes na frase'.format(frase.count('A')))
+print('Ela aparece pela primeira vez na posição {}'.format(frase.find('A')))   #find(encontrar, procurar)
+print('Ela aparece pela ultima vez na posição {}'.format(frase.rfind('A')))  #rfind(encontrar, procurar do lado direito)
