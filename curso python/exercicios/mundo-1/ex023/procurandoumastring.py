c8a1cf1ceaf3caf379c5a0ccbf42e9c4fac6efd4
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:28:15 -0300

    Update procurandoumastring.py

--- curso python/exercicios/mundo-1/ex023/procurandoumastring.py
+++ curso python/exercicios/mundo-1/ex023/procurandoumastring.py
@@ -1,2 +1,2 @@
-n = str(input('Qual é seu nome completo? ')).strip()
-print('Seu nome tem Silva? {}'.format('SILVA' in n.upper()))
+nome = str(input('Qual é seu nome completo? ')).strip()
+print('Seu nome tem Silva? {}'.format('SILVA' in nome.upper()))
