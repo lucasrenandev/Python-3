470fe728ae81a9e72866dc9af9c052497005ccac
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 14:02:14 -0300

    Update catetoehipotenusa.py

--- curso python/exercicios/mundo-1/ex015/catetoehipotenusa.py
+++ curso python/exercicios/mundo-1/ex015/catetoehipotenusa.py
@@ -1,13 +1,13 @@
-'''from math import hypot
+from math import hypot
 co = float(input("Digite o cateto oposto: "))
 ca = float(input("Digite o cateto adjacente: "))
 hi = hypot(co, ca)
-print("A hipotenusa vai medir {:.2f}".format(hi))'''
+print("A hipotenusa vai medir {:.2f}".format(hi))
 
-co = float(input("Digite o cateto oposto: "))
+'''co = float(input("Digite o cateto oposto: "))
 ca = float(input("Digite o cateto adjacente: "))
 hi = (co ** 2 + ca ** 2 ) ** (1/2)
-print("A hipotenusa vai medir {:.2f}".format(hi))
+print("A hipotenusa vai medir {:.2f}".format(hi))'''
 
 '''import math
 co = float(input("Digite o cateto oposto: "))
