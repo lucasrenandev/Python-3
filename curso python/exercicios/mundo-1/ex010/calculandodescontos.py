99ce4f20a6904f800588b9d5091c33cfbf722f37
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:46:21 -0300

    Update calculandodescontos.py

--- curso python/exercicios/mundo-1/ex010/calculandodescontos.py
+++ curso python/exercicios/mundo-1/ex010/calculandodescontos.py
@@ -1,3 +1,3 @@
 preço = float(input('Qual o preço do produto? R$ '))
-desco = preço - (preço * 5/ 100)
-print('O preço do produto R${:.2f} com desconto de 5% é igual a R${:.2f}.'.format(preço, desco))
+desco = preço - (preço * 5 / 100)
+print('O preço do produto é R${:.2f} com desconto de 5% ficará R${:.2f}.'.format(preço, desco))
