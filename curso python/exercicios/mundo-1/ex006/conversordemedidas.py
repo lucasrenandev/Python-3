0b5f15cd7ca97bccb136a41798bd191a2676adef
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:33:44 -0300

    Update conversordemedidas.py

--- curso python/exercicios/mundo-1/ex006/conversordemedidas.py
+++ curso python/exercicios/mundo-1/ex006/conversordemedidas.py
@@ -1,4 +1,4 @@
 medida = float(input('Digite uma medida: '))
 cm = medida * 100
 mm = medida * 1000
-print('A medida de {}m corresponde a {:.0f}cm e {:.0f}mm'.format(medida, cm, mm,))
+print('A medida de {}m² corresponde a {:.0f}cm e {:.0f}mm'.format(medida, cm, mm,))
