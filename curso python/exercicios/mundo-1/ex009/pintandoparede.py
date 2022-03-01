4c576b31cb9714001940fc6f2f1990b5f8dad4bb
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-01 13:41:41 -0300

    Update pintandoparede.py

--- curso python/exercicios/mundo-1/ex009/pintandoparede.py
+++ curso python/exercicios/mundo-1/ex009/pintandoparede.py
@@ -3,4 +3,4 @@ alt = float(input('Altura da parede: '))
 area = larg * alt
 print('Sua parede tem a dimensão de {}x{} e a sua area é de {}m²'.format(larg, alt, area))
 tinta = area / 2
-print('Para pintar essa parede você precisara de {}l de tinta: '.format(tinta))
+print('Para pintar essa parede você precisará de {}L de tinta: '.format(tinta))
