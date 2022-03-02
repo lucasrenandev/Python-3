b2c7d0ecb8f3b53f9a7d833f88e9b54e263783e0
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-02 20:02:35 -0300

    Update mediadoaluno.py

--- curso python/exercicios/mundo-2/ex038/mediadoaluno.py
+++ curso python/exercicios/mundo-2/ex038/mediadoaluno.py
@@ -1,10 +1,10 @@
 n1 = float(input("Primeira nota: "))
 n2 = float(input("Segunda nota: "))
-media = (n1 + n2) / 2
-print("Tirando {:.1f} e {:.1f}, a média do aluno é {:.1f}".format(n1, n2, media))
-if media >= 7:
-    print("O aluno está APROVADO")
-elif media < 7 and media >= 5:
-    print("O aluno está em RECUPERAÇÂO")
+média = (n1 + n2) / 2
+print("A média do aluno é {}".format(média))
+if média >= 7:
+    print("O aluno está APROVADO!")
+elif média < 7 and média >= 5:
+    print("O aluno está em RECUPERAÇÂO!")
 else:
     print("O aluno está REPROVADO.")
