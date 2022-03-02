3d2c8b8d80893085d192b39e0453d3428201d744
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-02 20:15:38 -0300

    Update indicedemassacorporal.py

--- curso python/exercicios/mundo-2/ex041/indicedemassacorporal.py
+++ curso python/exercicios/mundo-2/ex041/indicedemassacorporal.py
@@ -1,15 +1,15 @@
-peso = float(input("Qual é o seu peso? (Kg) "))
+peso = float(input("Qual é o peso da pessoa? (Kg) "))
 altura = float(input("Qual a sua altura? (m) "))
 imc = peso / (altura ** 2)
-print("O IMC dessa pessoa é de {:.1f}".format(imc))
+print("O IMC dessa pessoa é de {:.2f}".format(imc))
 if imc < 18.5:
     print("Abaixo do peso")
-elif 18.5 <= imc < 25:
+elif imc >= 18.5 and imc < 25:
     print("Peso ideal")
 elif imc >= 25 and imc < 30:
     print("Sobrepeso")
-elif 30 <= imc < 40:
+elif imc >= 30 and imc < 40:
     print("Obesidade")
 else:
-    print("Obesidade Mórbida")
+    print("Obesidade mórbida")
    
