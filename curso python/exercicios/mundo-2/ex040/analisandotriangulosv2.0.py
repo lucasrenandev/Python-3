44ac26c12f710b694efbe3fc4be3d0ff6b8e8a35
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-02 20:09:22 -0300

    Update analisandotriangulosv2.0.py

--- curso python/exercicios/mundo-2/ex040/analisandotriangulosv2.0.py
+++ curso python/exercicios/mundo-2/ex040/analisandotriangulosv2.0.py
@@ -2,7 +2,7 @@ r1 = float(input('Primeiro segmento: '))
 r2 = float(input('Segundo segmento: '))
 r3 = float(input('Terceiro segmento: '))
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
-    print("Os segmento acima pode formar um triângulo", end=' ')
+    print("Os segmentos acima podem formar um triângulo:", end=' ')
     if r1 == r2 == r3:
         print("EQUILÁTERO!")
     elif r1 != r2 != r3 != r1:
@@ -10,4 +10,4 @@ if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     else:
         print("ISÓSCELES")
 else:
-    print("Os segmento acima não podem formar um triângulo")
+    print("Os segmentos não podem formar um triângulo")
