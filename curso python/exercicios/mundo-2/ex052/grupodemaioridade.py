e64d0dd2d00b6c59401f4d112a59b476c1510e55
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-05 13:56:23 -0300

    Update grupodemaioridade.py

--- curso python/exercicios/mundo-2/ex052/grupodemaioridade.py
+++ curso python/exercicios/mundo-2/ex052/grupodemaioridade.py
@@ -3,7 +3,7 @@ atual = date.today().year
 totmaior = 0
 totmenor = 0
 for pess in range (1, 8):
-    nasc = int(input('Em que ano a {}° pessoa nasceu? '.format(pess)))
+    nasc = int(input('Em que ano a {}ª pessoa nasceu? '.format(pess)))
     idade = atual - nasc
     if idade >= 18:
         totmaior += 1
