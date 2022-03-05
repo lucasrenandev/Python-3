4808a20ee6c39a5ba80ae32c1839fc7db90b73f7
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-05 13:57:26 -0300

    Update maioremenordasequencia.py

--- curso python/exercicios/mundo-2/ex053/maioremenordasequencia.py
+++ curso python/exercicios/mundo-2/ex053/maioremenordasequencia.py
@@ -1,7 +1,7 @@
 maior = 0
 menor = 0
 for pess in range (1, 6):
-    peso = float(input('Peso da {}° pessoa: '.format(pess)))
+    peso = float(input('Peso da {}ª pessoa: '.format(pess)))
     if pess == 1:
         maior = peso
         menor = peso
