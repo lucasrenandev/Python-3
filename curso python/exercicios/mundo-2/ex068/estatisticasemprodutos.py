5268ee1f35e4593e6260f0e681bdc966cb1a0b3e
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-14 19:06:33 -0300

    Create estatisticasemprodutos.py

---
+++ curso python/exercicios/mundo-2/ex068/estatisticasemprodutos.py
@@ -0,0 +1,27 @@
+totgasto = totmil = menor = cont = 0
+barato = ''
+
+print('{:=^40}'.format('LOJA BARATÃO'))
+
+while True:
+    produto = str(input('Nome do produto: '))
+    preço = float(input('Preço: R$ '))
+    cont += 1
+    totgasto += preço
+    if preço > 1000:
+        totmil += 1
+    if cont == 1 or preço < menor:
+        menor = preço
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        break
+
+print('{:=^40}'.format('Fim do programa!'))
+
+print(f'Total gasto na compra: R${totgasto:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00! ')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
+
