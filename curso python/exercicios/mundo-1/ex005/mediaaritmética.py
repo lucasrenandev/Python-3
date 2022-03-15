a81d73149fbff841786f28d08e7ee8ee628d619f
Lucas Renan <lucasrenandns96@gmail.com>
2022-03-14 23:14:17 -0300

    Update mediaaritmética.py

--- curso python/exercicios/mundo-1/ex005/mediaaritmética.py
+++ curso python/exercicios/mundo-1/ex005/mediaaritmética.py
@@ -1,7 +1,6 @@
 n1 = float(input('Primeira nota do aluno: '))
 n2 = float(input('Segunda nota do aluno: '))
-media = (n1 + n2)/2
-print('A média entre',n1 ,'e', n2, 'é igual a', media)
-#print('A média entre {:.1f} e {:.1f} é igual a {:.1f}'.format(n1, n2, media))
+média = (n1 + n2)/2
+print('A média entre {:.2f} e {:.2f} é igual a {:.2f}'.format(n1, n2, média))
 
 
